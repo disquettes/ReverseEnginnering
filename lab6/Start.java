9332278f98e1c60d795e7a4c71c4bc8895af811a
z3r0 <z3r0@localhost.localdomain>
2017-03-28 11:03:32 -0800

    Commit message

---
+++ lab6/Start.java
@@ -0,0 +1,18 @@
+
+public class Start {
+	
+	public static void main(String[] strings){
+		Figura d = new Dreptunghi(2,2);
+		//System.out.println(d.aria());
+		
+		//System.out.println(d.perimetru());
+		
+		d = (Dreptunghi)(d);
+		System.out.println( ((Dreptunghi) d).diagonala());
+		
+		
+	System.out.println(Math.sqrt(4+4));
+			
+	}
+
+}
